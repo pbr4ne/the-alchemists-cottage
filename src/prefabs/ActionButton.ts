0eef0b6e5a4db17dfe6ecdd34ea893072b55307e
pbr4ne <pbr4ne@protonmail.com>
2024-07-24 23:03:17 -0700

    refactor: encapsulate button into its own prefab

---
+++ src/prefabs/ActionButton.ts
@@ -0,0 +1,101 @@
+import Phaser from 'phaser';
+
+export default class ActionButton extends Phaser.GameObjects.Container {
+    private outlineGraphics: Phaser.GameObjects.Graphics;
+    private fillGraphics: Phaser.GameObjects.Graphics;
+    private buttonText: Phaser.GameObjects.Text;
+    private buttonWidth: number;
+    private buttonHeight: number;
+    private buttonOutlineWidth: number;
+    private kettleProgress: number = 0;
+
+    constructor(scene: Phaser.Scene, x: number, y: number, label: string, onClick: () => void) {
+        super(scene, x, y);
+
+        this.buttonWidth = 200;
+        this.buttonHeight = 50;
+        this.buttonOutlineWidth = 5;
+
+        this.outlineGraphics = scene.add.graphics();
+        this.fillGraphics = scene.add.graphics();
+
+        this.buttonText = scene.add.text(0, 0, label, {
+            fontFamily: 'Arvo',
+            fontSize: '24px',
+            color: '#283618',
+        });
+        this.buttonText.setOrigin(0.5, 0.5);
+
+        this.add(this.fillGraphics);
+        this.add(this.outlineGraphics);
+        this.add(this.buttonText);
+
+        this.setSize(this.buttonWidth, this.buttonHeight);
+        this.setInteractive({ useHandCursor: true });
+
+        this.on('pointerover', this.onButtonHover, this);
+        this.on('pointerout', this.onButtonOut, this);
+        this.on('pointerdown', () => {
+            this.onButtonClick(onClick);
+        }, this);
+
+        scene.add.existing(this);
+        this.drawButton(0x283618);
+    }
+
+    private drawButton(outlineColor: number) {
+        const x = -this.buttonWidth / 2;
+        const y = -this.buttonHeight / 2;
+
+        this.outlineGraphics.clear();
+        this.outlineGraphics.lineStyle(this.buttonOutlineWidth, outlineColor, 1);
+        this.outlineGraphics.strokeRect(x, y, this.buttonWidth, this.buttonHeight);
+
+        this.fillGraphics.clear();
+        if (this.kettleProgress > 0) {
+            this.fillGraphics.fillStyle(0xffb703, 1);
+            this.fillGraphics.fillRect(x, y, this.kettleProgress, this.buttonHeight);
+        }
+    }
+
+    private onButtonHover() {
+        this.drawButton(0xbc6c25);
+    }
+
+    private onButtonOut() {
+        this.drawButton(0x283618);
+    }
+
+    private onButtonClick(onClick: () => void) {
+        let duration = 5000;
+        this.scene.tweens.add({
+            targets: this,
+            kettleProgress: { from: 0, to: this.buttonWidth },
+            duration,
+            onUpdate: () => {
+                this.drawButton(0xbc6c25);
+            },
+            onComplete: () => {
+                this.kettleProgress = 0;
+                this.drawButton(0x283618);
+                this.setVisible(false); // Hide the button when it finishes filling
+                onClick();
+            }
+        });
+    }
+
+    public resetButton(label: string) {
+        this.buttonText.setText(label);
+        this.setVisible(true);
+        this.setAlpha(0); // Start from 0 alpha for fade-in effect
+        this.scene.tweens.add({
+            targets: this,
+            alpha: 1,
+            duration: 1000
+        });
+    }
+
+    public getButtonText(): string {
+        return this.buttonText.text;
+    }
+}
--- src/scenes/Prologue.ts
+++ src/scenes/Prologue.ts
@@ -1,16 +1,11 @@
 import Phaser from "phaser";
 import FadeScript from "../scripts/FadeScript";
+import ActionButton from "../prefabs/ActionButton";
 import { checkUrlParam } from "../utilities/GameUtils";
 
 export default class Prologue extends Phaser.Scene {
 
-    private buttonOutlineGraphics!: Phaser.GameObjects.Graphics;
-    private buttonFillGraphics!: Phaser.GameObjects.Graphics;
-    private buttonWidth: number = 200;
-    private buttonHeight: number = 50;
-    private buttonOutlineWidth: number = 5;
-
-    private kettleProgress: number = 0;
+    private button!: ActionButton;
     private textBox!: Phaser.GameObjects.Text;
     private textContent1: string = "You wake up to the sound of a tea kettle whistling. The bed you're in is warm and inviting. You feel sad for some reason, though you're not sure why.";
     private buttonContent1: string = "Pick up kettle";
@@ -21,8 +16,7 @@ export default class Prologue extends Phaser.Scene {
     private currentTextContent: string;
     private textIndex: number = 0;
     private typingEvent!: Phaser.Time.TimerEvent;
-    private buttonContainer!: Phaser.GameObjects.Container;
-    private buttonText!: Phaser.GameObjects.Text;
+    private buttonWidth: number = 200;
 
     constructor() {
         super("Prologue");
@@ -35,28 +29,6 @@ export default class Prologue extends Phaser.Scene {
         const centerX = this.cameras.main.width / 2;
         const centerY = this.cameras.main.height / 2;
 
-        this.buttonOutlineGraphics = this.add.graphics();
-        this.buttonFillGraphics = this.add.graphics();
-
-        this.drawButton(0x283618);
-
-        this.buttonText = this.add.text(0, 0, this.buttonContent1, {
-            fontFamily: 'Arvo',
-            fontSize: '24px',
-            color: '#283618',
-        });
-        this.buttonText.setOrigin(0.5, 0.5);
-
-        this.buttonContainer = this.add.container(centerX, centerY + 100, [this.buttonFillGraphics, this.buttonOutlineGraphics, this.buttonText]);
-
-        this.buttonContainer.setSize(this.buttonWidth, this.buttonHeight);
-        this.buttonContainer.setInteractive({ useHandCursor: true });
-        this.buttonContainer.setAlpha(0);
-
-        this.buttonContainer.on('pointerover', this.onButtonHover, this);
-        this.buttonContainer.on('pointerout', this.onButtonOut, this);
-        this.buttonContainer.on('pointerdown', this.onButtonClick, this);
-
         this.textBox = this.add.text(centerX - this.buttonWidth, centerY - 150, '', {
             fontFamily: 'Arvo',
             fontSize: '24px',
@@ -64,7 +36,7 @@ export default class Prologue extends Phaser.Scene {
             align: 'left',
             wordWrap: { width: this.buttonWidth * 2, useAdvancedWrap: true }
         });
-        this.textBox.setOrigin(0, 0);
+        this.textBox.setOrigin(0, 0); // Set origin to top-left to avoid moving
 
         let delay = 50;
         if (checkUrlParam("fast", "true")) {
@@ -76,22 +48,17 @@ export default class Prologue extends Phaser.Scene {
             callbackScope: this,
             loop: true
         });
-    }
 
-    private drawButton(outlineColor: number) {
-        const x = -this.buttonWidth / 2;
-        const y = -this.buttonHeight / 2;
-        const outlineWidth = this.buttonOutlineWidth;
-
-        this.buttonOutlineGraphics.clear();
-        this.buttonOutlineGraphics.lineStyle(outlineWidth, outlineColor, 1);
-        this.buttonOutlineGraphics.strokeRect(x, y, this.buttonWidth, this.buttonHeight);
-
-        this.buttonFillGraphics.clear();
-        if (this.kettleProgress > 0) {
-            this.buttonFillGraphics.fillStyle(0xffb703, 1);
-            this.buttonFillGraphics.fillRect(x, y, this.kettleProgress, this.buttonHeight);
-        }
+        this.button = new ActionButton(this, centerX, centerY + 100, this.buttonContent1, () => {
+            this.fadeOutButtonAndText(() => {
+                if (this.button.getButtonText() === this.buttonContent2) {
+                    this.renderNewText(this.textContent3);
+                } else {
+                    this.renderNewText(this.textContent2);
+                }
+            });
+        });
+        this.button.setAlpha(0);
     }
 
     private renderText() {
@@ -126,47 +93,11 @@ export default class Prologue extends Phaser.Scene {
     }
 
     private fadeInButton(newLabel: string) {
-        this.buttonText.setText(newLabel);
-        this.buttonContainer.setVisible(true);
-        new FadeScript(this, this.buttonContainer as Phaser.GameObjects.Container & Phaser.GameObjects.Components.Alpha, true, 1000);
-    }
-
-    private onButtonHover() {
-        this.drawButton(0xbc6c25);
-    }
-
-    private onButtonOut() {
-        this.drawButton(0x283618);
-    }
-
-    private onButtonClick() {
-        let duration = 5000;
-        if (checkUrlParam("fast", "true")) {
-            duration = 500;
-        }
-        this.tweens.add({
-            targets: this,
-            kettleProgress: { from: 0, to: this.buttonWidth },
-            duration,
-            onUpdate: () => {
-                this.drawButton(0xbc6c25);
-            },
-            onComplete: () => {
-                this.kettleProgress = 0;
-                this.drawButton(0x283618);
-                this.fadeOutButtonAndText(() => {
-                    if (this.buttonText.text === this.buttonContent2) {
-                        this.renderNewText(this.textContent3);
-                    } else {
-                        this.renderNewText(this.textContent2);
-                    }
-                });
-            }
-        });
+        this.button.resetButton(newLabel);
     }
 
     private fadeOutButtonAndText(callback: () => void) {
-        new FadeScript(this, this.buttonContainer as Phaser.GameObjects.Container & Phaser.GameObjects.Components.Alpha, false, 3000);
+        new FadeScript(this, this.button as unknown as Phaser.GameObjects.Container & Phaser.GameObjects.Components.Alpha, false, 3000);
         new FadeScript(this, this.textBox as Phaser.GameObjects.Text & Phaser.GameObjects.Components.Alpha, false, 3000, () => {
             this.textBox.alpha = 1;
             callback();

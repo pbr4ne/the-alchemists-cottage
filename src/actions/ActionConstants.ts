e4209544c85313002eaf82aa44e72cef1e5b42df
pbr4ne <pbr4ne@protonmail.com>
2024-07-25 14:17:42 -0700

    feat: use fillDuration for actions

--- src/actions/ActionConstants.ts
+++ src/actions/ActionConstants.ts
@@ -1,10 +1,9 @@
+import { getFillDuration } from "../utilities/Timing";
 import Action from "./Action";
-
-export const TIME_SHORT: number = 5000;
 
 //prologue
 export const PICK_UP_KETTLE: string = "Pick up kettle";
 export const GO_TO_KITCHEN: string = "Go to kitchen";
 
-export const KETTLE_ACTION = new Action("kettle", PICK_UP_KETTLE, PICK_UP_KETTLE, TIME_SHORT);
-export const KITCHEN_ACTION = new Action("kitchen", GO_TO_KITCHEN, GO_TO_KITCHEN, TIME_SHORT);
+export const KETTLE_ACTION = new Action("kettle", PICK_UP_KETTLE, PICK_UP_KETTLE, getFillDuration());
+export const KITCHEN_ACTION = new Action("kitchen", GO_TO_KITCHEN, GO_TO_KITCHEN, getFillDuration());

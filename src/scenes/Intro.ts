62fdd57c246acd325afc442247604c9514ff588a
pbr4ne <pbr4ne@protonmail.com>
2024-07-24 17:31:50 -0700

    feat: tweak delay for fadeout

--- src/scenes/Intro.ts
+++ src/scenes/Intro.ts
@@ -37,7 +37,7 @@ export default class Intro extends BaseScene {
 
         this.letterIndex = 0;
         this.time.addEvent({
-            delay: 100, // Delay in milliseconds
+            delay: 100,
             callback: this.addNextLetter,
             callbackScope: this,
             loop: true
@@ -47,19 +47,15 @@ export default class Intro extends BaseScene {
 	}
 
     addNextLetter() {
-        // Check if there are more letters to add
         if (this.letterIndex < this.fullText.length) {
-            // Add the next letter to the current text
             this.currentText += this.fullText[this.letterIndex];
             this.text.setText(this.currentText);
 
-            // Increment the index for the next letter
             this.letterIndex++;
         } else {
-            // Stop the timer once all letters are displayed
             this.time.removeAllEvents();
 
-            this.time.delayedCall(100, this.fadeOutText, [], this);
+            this.time.delayedCall(500, this.fadeOutText, [], this);
         }
     }
 
@@ -67,10 +63,9 @@ export default class Intro extends BaseScene {
         this.tweens.add({
             targets: this.text,
             alpha: 0,
-            duration: 3000, // Duration of the fade out in milliseconds
+            duration: 3000,
             ease: 'Power2',
             onComplete: () => {
-                // Optionally, do something after the fade out completes
                 this.text.destroy();
             }
         });

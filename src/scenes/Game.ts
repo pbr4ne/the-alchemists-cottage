41d516034514a200196c21249276d9a84e9e6d98
pbr4ne <pbr4ne@protonmail.com>
2024-07-24 21:46:38 -0700

    feat: kettle button

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -3,6 +3,14 @@ import FadeScript from "../scripts/FadeScript";
 
 export default class Game extends Phaser.Scene {
 
+    private buttonOutlineGraphics!: Phaser.GameObjects.Graphics;
+    private buttonFillGraphics!: Phaser.GameObjects.Graphics;
+    private buttonWidth: number = 200;
+    private buttonHeight: number = 50;
+    private buttonOutlineWidth: number = 5;
+
+    private kettleProgress: number = 0;
+
     constructor() {
         super("Game");
     }
@@ -13,14 +21,10 @@ export default class Game extends Phaser.Scene {
         const centerX = this.cameras.main.width / 2;
         const centerY = this.cameras.main.height / 2;
 
-        const buttonOutline = this.add.graphics();
-        const buttonWidth = 200;
-        const buttonHeight = 50;
-        const buttonRadius = 20;
-        const buttonOutlineWidth = 5;
+        this.buttonOutlineGraphics = this.add.graphics();
+        this.buttonFillGraphics = this.add.graphics();
 
-        buttonOutline.lineStyle(buttonOutlineWidth, 0x283618, 1); 
-        buttonOutline.strokeRoundedRect(-buttonWidth / 2, -buttonHeight / 2, buttonWidth, buttonHeight, buttonRadius);
+        this.drawButton(0x283618);
 
         const buttonText = this.add.text(0, 0, "Pick up kettle", {
             fontFamily: 'Arvo',
@@ -29,8 +33,54 @@ export default class Game extends Phaser.Scene {
         });
         buttonText.setOrigin(0.5, 0.5);
 
-        const buttonContainer = this.add.container(centerX, centerY, [buttonOutline, buttonText]);
+        const buttonContainer = this.add.container(centerX, centerY, [this.buttonFillGraphics, this.buttonOutlineGraphics, buttonText]);
+
+        buttonContainer.setSize(this.buttonWidth, this.buttonHeight);
+        buttonContainer.setInteractive({ useHandCursor: true });
+
+        buttonContainer.on('pointerover', this.onButtonHover, this);
+        buttonContainer.on('pointerout', this.onButtonOut, this);
+        buttonContainer.on('pointerdown', this.onButtonClick, this);
 
         new FadeScript(this, buttonContainer as unknown as Phaser.GameObjects.Container & Phaser.GameObjects.Components.Alpha, true, 3000);
-	}
+    }
+
+    private drawButton(outlineColor: number) {
+        const x = -this.buttonWidth / 2;
+        const y = -this.buttonHeight / 2;
+        const outlineWidth = this.buttonOutlineWidth;
+
+        this.buttonOutlineGraphics.clear();
+        this.buttonOutlineGraphics.lineStyle(outlineWidth, outlineColor, 1);
+        this.buttonOutlineGraphics.strokeRect(x, y, this.buttonWidth, this.buttonHeight);
+
+        this.buttonFillGraphics.clear();
+        if (this.kettleProgress > 0) {
+            this.buttonFillGraphics.fillStyle(0xffb703, 1);
+            this.buttonFillGraphics.fillRect(x, y, this.kettleProgress, this.buttonHeight);
+        }
+    }
+
+    private onButtonHover() {
+        this.drawButton(0xbc6c25);
+    }
+
+    private onButtonOut() {
+        this.drawButton(0x283618);
+    }
+
+    private onButtonClick() {
+        this.tweens.add({
+            targets: this,
+            kettleProgress: { from: 0, to: this.buttonWidth },
+            duration: 5000,
+            onUpdate: () => {
+                this.drawButton(0xbc6c25);
+            },
+            onComplete: () => {
+                this.kettleProgress = 0;
+                this.drawButton(0x283618);
+            }
+        });
+    }
 }
